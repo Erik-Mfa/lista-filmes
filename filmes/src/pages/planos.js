bb88d54dda9c902b16c24e8ccdf52c37558e3b74
Erik-Mfa <erikmfa190230@gmail.com>
2023-03-26 23:54:49 -0300

    planos

--- filmes/src/pages/planos.js
+++ filmes/src/pages/planos.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import Title from './../components/Title/index';
- 
-function Planos(){
-    return (
-        <div>
-            <Title
-                title={"Planos"}
-                text={"Veja abaixo os planos disponíveis:"} />
-            {/* <Card /> */}
-            <h1>Planos</h1>         
-        </div>
-    )   
+
+const planos = [{
+  "nome": "Plano 1",
+  "valor": "25$"
+
+},
+{
+  "nome": "Plano 2",
+  "valor": "50$"
+},
+{
+  "nome": "Plano 3",
+  "valor": "100$",
 }
- 
-export default Planos;
+]
+
+export default function Planos() {
+  return (
+    
+    <div className="container text-center">
+      <div class="row">
+        {planos.map((plano, i) => (
+          <div className="col" key={i.toString()}>
+            <div className="card">
+            <h1 className="card-title m-2">{plano.nome}</h1>
+              <div className="card-body">
+                <hr className="mt-0"></hr>
+                <h4 className="card-text">{plano.valor}</h4>
+              
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
